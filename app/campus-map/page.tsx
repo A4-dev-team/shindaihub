48a3d700db351b3a029bde55b9dd711c0088af94
yuto <yuto.0313king@icloud.com>
2024-02-27 21:28:12 +0900

    [fix]不要なimport削除

--- app/campus-map/page.tsx
+++ app/campus-map/page.tsx
@@ -2,9 +2,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaMapPin } from "react-icons/fa";
-import { Building, Campus } from "@/types";
+import { Building } from "@/types";
 import { constant } from "@/constant";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { SkeltonImage } from "@/components/SkeltonImage";
 
