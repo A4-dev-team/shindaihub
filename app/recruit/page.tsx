e06807416eb69846480c35f58484e1de5445d0c4
Shota Mizusaki <nrxg129@gmail.com>
2024-03-15 00:02:25 +0900

    編集

---
+++ app/recruit/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+    return (
+        <div className="flex flex-col items-center w-full">
+            <p>
+                A4では、通年で新メンバーを募集しています。<br/>
+                興味のある方は、以下の資料をご覧ください。
+            </p>
+            <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAF8jjzX-pQ&#x2F;bBGV55C-9mOwnXiy-C20tQ&#x2F;view?utm_content=DAF8jjzX-pQ&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener" className="text-bluenormal mt-2">A4 団体説明・採用資料</a>
+            <div  style={{width: "90%", height: "auto", position: "relative", padding: "56.2500% 0 0", boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", marginTop: "0.8em", marginBottom: "0.9em", overflow: "hidden", borderRadius: "8px", willChange: "transform" }}>
+                <iframe loading="lazy" style={{ position: "absolute", width: "100%", height: "100%", top: "0", left: "0", border: "none", padding: "0", margin: "0" }} src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAF8jjzX-pQ&#x2F;bBGV55C-9mOwnXiy-C20tQ&#x2F;view?embed" allowFullScreen={true} allow="fullscreen">
+                </iframe>
+            </div>
+        </div>
+    );
+}
