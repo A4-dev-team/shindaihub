1d9b3880f7dfd98bd1b2499b2aa10eba184cdc79
Keisuke05410 <keisuke3469@gmail.com>
2024-03-22 21:50:49 +0900

    [update]お知らせ一覧の実装と個別ページの実装

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,76 +1,43 @@
-'use client'
-import { Viewer, Worker } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import fs from "fs";
+import path from "path";
 
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { ToolbarProps } from '@react-pdf-viewer/default-layout';
-import { ReactElement } from 'react'
-import { ToolbarSlot } from '@react-pdf-viewer/toolbar';
+import news_info from "../../public/news/news_info.json";
+import Link from "next/link";
 
-const renderToolbar = (Toolbar: (props: ToolbarProps) => ReactElement) => (
-  <Toolbar>
-      {(slots: ToolbarSlot) => {
-          const {
-              CurrentPageInput,
-              Download,
-              EnterFullScreen,
-              GoToNextPage,
-              GoToPreviousPage,
-              NumberOfPages,
-              Print,
-              ShowSearchPopover,
-              Zoom,
-              ZoomIn,
-              ZoomOut,
-          } = slots;
-          return (
-              <div
-                  style={{
-                      alignItems: 'center',
-                      display: 'flex',
-                      width: '100%',
-                  }}
-              >
-                  <div style={{ padding: '0px 2px' }}>
-                      <ShowSearchPopover />
-                  </div>
-                  <div style={{ padding: '0px 2px' }}>
-                      <ZoomOut />
-                  </div>
-                  <div style={{ padding: '0px 2px' }}>
-                      <Zoom />
-                  </div>
-                  <div style={{ padding: '0px 2px' }}>
-                      <ZoomIn />
-                  </div>
-                  <div style={{ padding: '0px 2px', marginLeft: 'auto' }}>
-                      <EnterFullScreen />
-                  </div>
-              </div>
-          );
-      }}
-  </Toolbar>
-);
+export default async function Page() {
+    const data = news_info.data;
 
-export default function Page() {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin({
-    renderToolbar,
-});
-
-  return (
-    <>
-      <div className='my-10'>
-        <h1 className='text-2xl'>※サービスに関する重要なお知らせ</h1>
-      </div>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js">
-          <Viewer
-              fileUrl="doc.pdf"
-              plugins={[
-                  defaultLayoutPluginInstance,
-                ]}
-                />
-      </Worker>
-    </>
-  );
+    return (
+        <div className="container">
+            <div className="flex flex-col items-center">
+                <div>
+                    <p className="text-3xl p-5">お知らせ詳細</p>
+                </div>
+                <div className="w-full sm:w-2/3">
+                    {data.map((doc, index) => (
+                        <Link
+                            key={index}
+                            href={{
+                                pathname: `/news/${doc.pdf}`,
+                                query: {
+                                    title: doc.title,
+                                    date: doc.date,
+                                    pdf: doc.pdf,
+                                },
+                            }}
+                        >
+                            <div key={doc.pdf} className="p-5 border-y">
+                                <p className="text-gray-500">{doc.date}</p>
+                                <div className="flex items-center justify-center">
+                                    <h1 className="text-2xl font-bold text-bluenormal">
+                                        {doc.title}
+                                    </h1>
+                                </div>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
