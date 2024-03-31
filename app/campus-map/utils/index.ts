27d7ac5a927fbca8225bfd4f3e1efdc73863ad1b
Condy <132919366+yu03-dev@users.noreply.github.com>
2024-04-01 00:56:47 +0900

    [fix]正規表現を正しくエスケープするように修正

--- app/campus-map/utils/index.ts
+++ app/campus-map/utils/index.ts
@@ -14,14 +14,19 @@ const findBuildingByKeyword = (campusId: number, keyword: string) => {
     );
 };
 
+const escapeRegExp = (string: string) => {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+};
+
 const findAllBuildingsByKeyword = (campusId: number, keyword: string) => {
     if (keyword == "") {
         return constant.buildingList.filter(
             (building) => building.campusId == campusId
         );
     }
+    const escapedKeyword = escapeRegExp(keyword);
     const matches = constant.buildingList.filter((building) => {
-        const regex = new RegExp(keyword, "gi");
+        const regex = new RegExp(escapedKeyword, "gi");
         return (
             building.buildingName.match(regex) && building.campusId == campusId
         );
