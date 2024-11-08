279a64683b3adf7ea1bd529a4bfd8465a62f981d
Keisuke05410 <keisuke3469@gmail.com>
2024-11-09 08:51:55 +0900

    環境変数が読み込まれないのでtestように環境変数を出力

--- app/campus-map/components/GoogleMapIFrame.tsx
+++ app/campus-map/components/GoogleMapIFrame.tsx
@@ -1,28 +1,30 @@
 type GoogleMapIFrameProps = {
-    lat: number;
-    lng: number;
-    q?: string;
+  lat: number;
+  lng: number;
+  q?: string;
 };
 
 const GoogleMapIFrame = (props: GoogleMapIFrameProps) => {
-    const { lat, lng, q } = props;
-    const center = `${lat}, ${lng}`;
+  const { lat, lng, q } = props;
+  const center = `${lat}, ${lng}`;
 
-    return (
-        <div className="w-full">
-            <iframe
-                width="100%"
-                height="600px"
-                style={{ border: 0 }}
-                loading="lazy"
-                allowFullScreen
-                referrerPolicy="no-referrer-when-downgrade"
-                src={`https://www.google.com/maps/embed/v1/place?key=${
-                    process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY
-                }&q=${q || center}&center=${center}&zoom=18`}
-            ></iframe>
-        </div>
-    );
+  console.log(process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY);
+
+  return (
+    <div className="w-full">
+      <iframe
+        width="100%"
+        height="600px"
+        style={{ border: 0 }}
+        loading="lazy"
+        allowFullScreen
+        referrerPolicy="no-referrer-when-downgrade"
+        src={`https://www.google.com/maps/embed/v1/place?key=${
+          process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY
+        }&q=${q || center}&center=${center}&zoom=18`}
+      ></iframe>
+    </div>
+  );
 };
 
 export { GoogleMapIFrame };
